6cb14f897885f0f8fb4e9bd0031d22131b5766f8
Nabi Gauda <69577335+b34st-n4b1@users.noreply.github.com>
2020-12-03 14:43:19 +0530

    Add files via upload

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBD6t88VEwKUnxTBBQTU0Cc5lq9KLtSyFA",
+    authDomain: "mygitapp-64166.firebaseapp.com",
+    databaseURL: "https://mygitapp-64166.firebaseio.com",
+    projectId: "mygitapp-64166",
+    storageBucket: "mygitapp-64166.appspot.com",
+    messagingSenderId: "666809736149",
+    appId: "1:666809736149:web:958dc61044da4453b13b5c",
+    measurementId: "G-WFY973P94T"
+  };
